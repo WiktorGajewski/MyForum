8e7952b2a43bfa6df7d0ae19feafb173ae8f7c93
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-07-26 15:07:27 +0200

    implemented removing members from the guild by guildmaster

--- MyForum/Pages/Guilds/Details.cshtml.cs
+++ MyForum/Pages/Guilds/Details.cshtml.cs
@@ -1,22 +1,31 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyForum.Core;
 using MyForum.Data.Interfaces;
+using System.Security.Claims;
 
 namespace MyForum.Pages.Guilds
 {
     public class DetailsModel : PageModel
     {
         private readonly IGuildRepostiory guildData;
+        private readonly IUserRepository userData;
 
         [TempData]
         public string Message { get; set; }
 
         public Guild Guild { get; set; }
 
-        public DetailsModel(IGuildRepostiory guildData)
+        public string currentUserId { get; set; }
+
+        public DetailsModel(IGuildRepostiory guildData, IUserRepository userData,
+            IHttpContextAccessor httpContextAccessor)
         {
             this.guildData = guildData;
+            this.userData = userData;
+            currentUserId = httpContextAccessor
+                .HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
         }
 
         public IActionResult OnGet(int guildId)
@@ -30,6 +39,22 @@ namespace MyForum.Pages.Guilds
             }
 
             return Page();
+        }
+
+        public IActionResult OnPost(int guildId, string memberId)
+        {
+            Guild = guildData.GetByIdWithMembersData(guildId);
+
+            if (Guild.GuildmasterId != currentUserId)
+            {
+                TempData["Message"] = "You have no permission to delete members of this guild!";
+                return RedirectToPage("./NotFound");
+            }
+
+            guildData.RemoveMember(Guild.Id, memberId);
+
+            TempData["Message"] = "Member removed from the Guild";
+            return RedirectToPage();
         }
     }
 }
