9a02249185acda583a3885c523140ec391404733
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2023-08-06 10:46:00 +0200

    Add Required to User.Nickname

---
+++ MyForum.Data/Migrations/20210719101758_AddUserNicknameRequired.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyForum.Data.Migrations
+{
+    public partial class AddUserNicknameRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+        }
+    }
+}
